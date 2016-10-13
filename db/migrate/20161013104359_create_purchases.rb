f1bc7662fc3142fa4389daab27055b42a7692dcb
Dante Miranda <dante.alighierimds@gmail.com>
2016-10-13 10:35:29 -0300

    Create purchases model

---
+++ db/migrate/20161013104359_create_purchases.rb
@@ -0,0 +1,15 @@
+class CreatePurchases < ActiveRecord::Migration[5.0]
+  def change
+    create_table :purchases do |t|
+      t.string :purchaser_name
+      t.string :item_description
+      t.float :item_price
+      t.integer :purchase_count
+      t.string :merchant_address
+      t.string :merchant_name
+      t.string :file_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161013104359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "purchaser_name"
+    t.string   "item_description"
+    t.float    "item_price"
+    t.integer  "purchase_count"
+    t.string   "merchant_address"
+    t.string   "merchant_name"
+    t.string   "file_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
